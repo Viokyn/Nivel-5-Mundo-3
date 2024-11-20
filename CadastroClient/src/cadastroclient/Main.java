7e093621f4d174fcbf6add8dfa9e52395176fd15
Filippe Markouizos Duarte <164840173+Viokyn@users.noreply.github.com>
2024-11-20 02:50:57 -0300

    Add files via upload

---
+++ CadastroClient/src/cadastroclient/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cadastroclient;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        CadastroClient2 cliente = new CadastroClient2();
+        cliente.Execute();
+    }
+}
